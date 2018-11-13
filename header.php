a76c24b210e84924f611a767451b104906ce18d0
Sean Wieler <sbwieler@gmail.com>
2018-11-12 21:36:38 -0800

    rework design

--- header.php
+++ header.php
@@ -16,9 +16,11 @@
 	<meta charset="<?php bloginfo( 'charset' ); ?>">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 	<link rel="profile" href="https://gmpg.org/xfn/11">
-	<link href="https://fonts.googleapis.com/css?family=Open+Sans" rel="stylesheet">
 	<link href="https://fonts.googleapis.com/css?family=Rock+Salt" rel="stylesheet">
-	<link href="https://fonts.googleapis.com/css?family=Special+Elite" rel="stylesheet">
+	<link href="https://fonts.googleapis.com/css?family=Noto+Sans|Noto+Serif" rel="stylesheet">
+	<link href="https://fonts.googleapis.com/css?family=Oswald:400,600,700" rel="stylesheet">
+	<link href="https://fonts.googleapis.com/css?family=Anton" rel="stylesheet">
+	<link href="https://fonts.googleapis.com/css?family=Nixie+One" rel="stylesheet">
 	<script src='https://www.google.com/recaptcha/api.js'></script>
 
 	<?php wp_head(); ?>
